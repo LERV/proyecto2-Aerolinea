e01f70075f7885a147d08d7d36e586dcfdcea66d
Nelson <nvgomez11@gmail.com>
2018-06-10 00:13:40 -0600

    classes listo

--- front-end/AirlineApp/app/src/main/java/com/luis/edward/airlineapp/DetalleClasses.java
+++ front-end/AirlineApp/app/src/main/java/com/luis/edward/airlineapp/DetalleClasses.java
@@ -1,8 +1,11 @@
 package com.luis.edward.airlineapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.util.ArrayList;
 
 public class DetalleClasses extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient googleApiClient;
+    private ImageView imageUser;
+    private TextView nameUser;
+    private TextView emailUser;
+    private View navHeader;
+
+    private ArrayList array_economy;
+    private ArrayList array_business;
+    private ArrayList array_premium;
+
+    private TextView txt1;
+    private TextView txt2;
+    private TextView txt3;
+    private TextView txt4;
+    private TextView txt5;
+    private TextView txt6;
+
+    private int num_class;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +80,65 @@ public class DetalleClasses extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //-----------------------------------------------------
+        //---------Para conectar a google silenciosamente y cargar foto nombre email
+        navHeader = navigationView.getHeaderView(0);
+        imageUser = (ImageView) navHeader.findViewById(R.id.imageViewGoogle_user);
+        nameUser = (TextView) navHeader.findViewById(R.id.nameGoogle_user);
+        emailUser = (TextView) navHeader.findViewById(R.id.emailGoogle_user);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
+        //-------------------------------------------------------
+        //-------------------------------------------------------
+
+        Intent intent = getIntent();
+        num_class = intent.getIntExtra("clase",0);
+        Log.d("num",String.valueOf(num_class));
+
+        array_economy = new ArrayList<String>();
+        array_business = new ArrayList<String>();
+        array_premium = new ArrayList<String>();
+        txt1 = findViewById(R.id.textView1);
+        txt2 = findViewById(R.id.textView2);
+        txt3 = findViewById(R.id.textView3);
+        txt4 = findViewById(R.id.textView4);
+        txt5 = findViewById(R.id.textView5);
+        txt6 = findViewById(R.id.textView6);
+
+        array_economy.add("6kg carry-on baggage");
+        array_economy.add("20kg normal baggage");
+        array_economy.add("Standard food");
+        array_economy.add("Standard drinks");
+        array_economy.add("Personalized entertainment");
+        array_economy.add("Changes are allowed with charges");
+
+        array_business.add("10kg carry-on baggage");
+        array_business.add("25kg normal baggage");
+        array_business.add("Selected food");
+        array_business.add("Alcoholic beverages");
+        array_business.add("Individual screen");
+        array_business.add("Changes are allowed with charges");
+
+        array_premium.add("15kg carry-on baggage");
+        array_premium.add("No normal baggage weight limit");
+        array_premium.add("Food and drink refill are allowed");
+        array_premium.add("All movies are unlocked");
+        array_premium.add("Bigger seat");
+        array_premium.add("Changes without charges");
+
+        llena();
+
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -50,6 +148,56 @@ public class DetalleClasses extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if(opr.isDone()){
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        }else{
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            nameUser.setText(account.getDisplayName());
+            emailUser.setText(account.getEmail());
+            //para cargar la foto de la persona
+            Glide.with(this).load(account.getPhotoUrl()).into(imageUser);
+
+        }else{
+            goLoginScreen();
+        }
+    }
+
+    public void log_out(){
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if(status.isSuccess()){
+                    goLoginScreen();
+                }else{
+                    Toast.makeText(DetalleClasses.this,"Session could not be closed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @Override
@@ -89,11 +237,44 @@ public class DetalleClasses extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
+            log_out();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void llena() {
+        if(num_class==1){
+            txt1.setText(array_economy.get(0).toString());
+            txt2.setText(array_economy.get(1).toString());
+            txt3.setText(array_economy.get(2).toString());
+            txt4.setText(array_economy.get(3).toString());
+            txt5.setText(array_economy.get(4).toString());
+            txt6.setText(array_economy.get(5).toString());
+        }
+        if(num_class==2){
+            txt1.setText(array_business.get(0).toString());
+            txt2.setText(array_business.get(1).toString());
+            txt3.setText(array_business.get(2).toString());
+            txt4.setText(array_business.get(3).toString());
+            txt5.setText(array_business.get(4).toString());
+            txt6.setText(array_business.get(5).toString());
+        }
+        if(num_class==3){
+            txt1.setText(array_premium.get(0).toString());
+            txt2.setText(array_premium.get(1).toString());
+            txt3.setText(array_premium.get(2).toString());
+            txt4.setText(array_premium.get(3).toString());
+            txt5.setText(array_premium.get(4).toString());
+            txt6.setText(array_premium.get(5).toString());
+        }
+    }
 }
+
